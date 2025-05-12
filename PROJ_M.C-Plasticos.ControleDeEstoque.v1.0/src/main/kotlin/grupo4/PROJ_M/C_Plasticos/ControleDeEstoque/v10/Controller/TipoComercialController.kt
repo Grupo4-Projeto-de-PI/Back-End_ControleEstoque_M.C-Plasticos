ca447c9766ee0c70c5a335ae03c2008d2e3233c2
MônicaJiuLea28 <monica.leamari@sptech.school>
2025-05-12 19:50:05 -0300

    Listar tipoComercial

--- PROJ_M.C-Plasticos.ControleDeEstoque.v1.0/src/main/kotlin/grupo4/PROJ_M/C_Plasticos/ControleDeEstoque/v10/Controller/TipoComercialController.kt
+++ PROJ_M.C-Plasticos.ControleDeEstoque.v1.0/src/main/kotlin/grupo4/PROJ_M/C_Plasticos/ControleDeEstoque/v10/Controller/TipoComercialController.kt
@@ -23,34 +23,34 @@ class TipoComercialController(val repositorio: TipoComercialRepositorio) {
     }
 
 
-    @PostMapping("/criar")
-    fun criarTipoComercial(@RequestBody novoTipo: TipoComercial): ResponseEntity<TipoComercial>{
-        novoTipo.id = null
-        val tipoComercial = repositorio.save(novoTipo)
-        return ResponseEntity.status(201).body(tipoComercial)
-    }
-
-    @DeleteMapping("/deletar/{id}")
-    fun deletarTipoComercial(@PathVariable id: Int):ResponseEntity<Void>{
-        if (repositorio.existsById(id)){
-            repositorio.deleteById(id)
-            return ResponseEntity.status(204).build()
-        }
-
-        return ResponseEntity.status(404).build()
-    }
-
-    @PatchMapping("/tipo/{id}/{tipo}")
-    fun atualizarTipoComercial(@PathVariable id: Int, @PathVariable tipo: String): ResponseEntity<TipoComercial>{
-
-        if (repositorio.existsById(id)) {
-            val tipoEncontrado = repositorio.findById(id).get()
-            tipoEncontrado.tipo = tipo
-            return ResponseEntity.status(200).body(tipoEncontrado)
-
-        }
-        return ResponseEntity.status(404).build()
-    }
+//    @PostMapping("/criar")
+//    fun criarTipoComercial(@RequestBody novoTipo: TipoComercial): ResponseEntity<TipoComercial>{
+//        novoTipo.id = null
+//        val tipoComercial = repositorio.save(novoTipo)
+//        return ResponseEntity.status(201).body(tipoComercial)
+//    }
+//
+//    @DeleteMapping("/deletar/{id}")
+//    fun deletarTipoComercial(@PathVariable id: Int):ResponseEntity<Void>{
+//        if (repositorio.existsById(id)){
+//            repositorio.deleteById(id)
+//            return ResponseEntity.status(204).build()
+//        }
+//
+//        return ResponseEntity.status(404).build()
+//    }
+//
+//    @PatchMapping("/tipo/{id}/{tipo}")
+//    fun atualizarTipoComercial(@PathVariable id: Int, @PathVariable tipo: String): ResponseEntity<TipoComercial>{
+//
+//        if (repositorio.existsById(id)) {
+//            val tipoEncontrado = repositorio.findById(id).get()
+//            tipoEncontrado.tipo = tipo
+//            return ResponseEntity.status(200).body(tipoEncontrado)
+//
+//        }
+//        return ResponseEntity.status(404).build()
+//    }
 
 
 
