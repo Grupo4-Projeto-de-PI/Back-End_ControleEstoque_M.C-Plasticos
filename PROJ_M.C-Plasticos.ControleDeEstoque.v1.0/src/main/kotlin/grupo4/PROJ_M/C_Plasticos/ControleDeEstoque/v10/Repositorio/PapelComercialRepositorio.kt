057add33d36c15b635854112dfc4e22f317571a2
MônicaJiuLea28 <monica.leamari@sptech.school>
2025-05-14 01:11:02 -0300

    Entidade PapelComercial e seus CRUDS

---
+++ PROJ_M.C-Plasticos.ControleDeEstoque.v1.0/src/main/kotlin/grupo4/PROJ_M/C_Plasticos/ControleDeEstoque/v10/Repositorio/PapelComercialRepositorio.kt
@@ -0,0 +1,7 @@
+package grupo4.PROJ_M.C_Plasticos.ControleDeEstoque.v10.Repositorio
+
+import grupo4.PROJ_M.C_Plasticos.ControleDeEstoque.v10.Entidades.PapelComercial
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface PapelComercialRepositorio: JpaRepository<PapelComercial, Int> {
+}
