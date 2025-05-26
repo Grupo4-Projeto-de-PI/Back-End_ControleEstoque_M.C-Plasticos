761090d572ee4110b256212a94c1fc2d4deb2773
joaojp06 <joao.antonio@sptech.school>
2025-05-25 23:26:27 -0300

    Construção dos testes unitários

--- PROJ_M.C-Plasticos.ControleDeEstoque.v1.0/src/main/kotlin/grupo4/PROJ_M/C_Plasticos/ControleDeEstoque/v10/Service/AtorComercialService.kt
+++ PROJ_M.C-Plasticos.ControleDeEstoque.v1.0/src/main/kotlin/grupo4/PROJ_M/C_Plasticos/ControleDeEstoque/v10/Service/AtorComercialService.kt
@@ -2,4 +2,19 @@ package grupo4.PROJ_M.C_Plasticos.ControleDeEstoque.v10.Service
 
 class AtorComercialService {
 
+    fun getTelefone(telefoneNovo: String): String {
+        if (telefoneNovo.length != 11) {
+            throw IllegalArgumentException("O telefone deve ter 11 algarismos")
+        }
+        return telefoneNovo
+}
+
+    fun getNome(nomeNovo: String): String {
+        if (nomeNovo.isBlank()) {
+            throw IllegalArgumentException("O nome não pode ser vazio")
+        }
+        return nomeNovo
+    }
+
+
 }
---
+++ PROJ_M.C-Plasticos.ControleDeEstoque.v1.0/src/test/kotlin/grupo4/PROJ_M/C_Plasticos/ControleDeEstoque/v10/LogPrecoProdutoServiceTest.kt
@@ -0,0 +1,43 @@
+package grupo4.PROJ_M.C_Plasticos.ControleDeEstoque.v10
+
+import grupo4.PROJ_M.C_Plasticos.ControleDeEstoque.v10.Entidades.LogPrecoProduto
+import grupo4.PROJ_M.C_Plasticos.ControleDeEstoque.v10.Entidades.Produto
+import grupo4.PROJ_M.C_Plasticos.ControleDeEstoque.v10.Service.LogPrecoProdutoService
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Test
+import java.math.BigDecimal
+import java.time.LocalDateTime
+
+class LogPrecoProdutoServiceTest {
+
+    val service = LogPrecoProdutoService()
+
+    @Test
+    fun `valores menor que 0 não devem ser aceitos`() {
+
+        assertThrows(IllegalArgumentException::class.java) {
+            service.getPrecoAlterado(-10.0)
+        }
+
+    }
+
+    @Test
+    fun `o preço novo deve ser diferente do antigo`() {
+
+        val precoAntigo = BigDecimal("10.00")
+        val precoNovo = BigDecimal("11.00")
+        val produto = Produto(/* inicialize conforme necessário */)
+        val logPreco = LogPrecoProduto(
+            precoAntigo = precoAntigo,
+            fkProduto = produto,
+            precoNovo = precoNovo,
+            dataAlteracao = LocalDateTime.now()
+        )
+
+        val resultado = service.getPrecoAlterado(logPreco.precoNovo!!.toDouble())
+
+        assert(resultado != precoAntigo)
+    }
+
+
+}
