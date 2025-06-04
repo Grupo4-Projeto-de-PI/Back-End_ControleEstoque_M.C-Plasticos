3296663b703f888a64ee27c8fbde04dd902f3e80
MônicaJiuLea28 <monica.leamari@sptech.school>
2025-06-03 22:39:37 -0300

    API Individual Clima

--- PROJ_M.C-Plasticos.ControleDeEstoque.v1.0/src/main/kotlin/grupo4/PROJ_M/C_Plasticos/ControleDeEstoque/v10/ApisIndividuais/monica/ClimaController.kt
+++ PROJ_M.C-Plasticos.ControleDeEstoque.v1.0/src/main/kotlin/grupo4/PROJ_M/C_Plasticos/ControleDeEstoque/v10/ApisIndividuais/monica/ClimaController.kt
@@ -1,16 +1,15 @@
 package grupo4.PROJ_M.C_Plasticos.ControleDeEstoque.v10.monica
 
+import grupo4.PROJ_M.C_Plasticos.ControleDeEstoque.v10.ApisIndividuais.monica.Clima
+import grupo4.PROJ_M.C_Plasticos.ControleDeEstoque.v10.ApisIndividuais.monica.ClimaService
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.RestTemplate
 import org.json.JSONObject
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/clima")
-class ClimaController {
+class ClimaController (private val service: ClimaService){
 
     private val apiKey = "3cc2ca5b51ee68307eecb53d90bb1e01"
 
@@ -37,4 +36,38 @@ class ClimaController {
             ResponseEntity.internalServerError().build()
         }
     }
+
+    @PostMapping
+    fun criar(@RequestBody dto: ClimaDto): ResponseEntity<Clima> {
+        val clima = dto.toEntity()
+        val salvo = service.salvar(clima)
+        return ResponseEntity.ok(salvo)
+    }
+
+    @GetMapping
+    fun listar(): ResponseEntity<List<Clima>> = ResponseEntity.ok(service.listar())
+
+    @GetMapping("/{id}")
+    fun buscar(@PathVariable id: Long): ResponseEntity<Clima> {
+        val clima = service.buscarPorId(id) ?: return ResponseEntity.notFound().build()
+        return ResponseEntity.ok(clima)
+    }
+
+    @PutMapping("/{id}")
+    fun atualizar(@PathVariable id: Long, @RequestBody dto: ClimaDto): ResponseEntity<Clima> {
+        val atualizado = service.atualizar(id, dto.toEntity()) ?: return ResponseEntity.notFound().build()
+        return ResponseEntity.ok(atualizado)
+    }
+
+    @DeleteMapping("/{id}")
+    fun deletar(@PathVariable id: Long): ResponseEntity<Void> {
+        service.deletar(id)
+        return ResponseEntity.noContent().build()
+    }
+
+    fun Clima.toDto() = ClimaDto(cidade, descricao, temperatura, umidade, vento)
+    fun ClimaDto.toEntity() = Clima(cidade = cidade, descricao = descricao, temperatura = temperatura, umidade = umidade, vento = vento)
+
+
+
 }
